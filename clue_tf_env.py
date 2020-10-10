348ac718a3fcdb43c383ed8a008fab2ca149314c
jeff012345 <jtmontic@gmail.com>
2020-10-10 16:33:51 -0400

    example code works

--- clue_tf_env.py
+++ clue_tf_env.py
@@ -22,18 +22,18 @@ class ClueCardCategoryEnv(py_environment.PyEnvironment):
     _tries: int
     
     def __init__(self, num_of_cards = 6):
+        # TODO change specs to take all three cards and observations?
         self._num_of_cards = num_of_cards
         self._action_spec = array_spec.BoundedArraySpec(shape=(1,), dtype=np.int32, 
                                                         minimum=0, 
                                                         maximum=num_of_cards - 1, 
                                                         name='action')
 
-        self._observation_spec = array_spec.BoundedArraySpec(shape=(1,), dtype=np.int32, 
+        self._observation_spec = array_spec.BoundedArraySpec(shape=(num_of_cards,), dtype=np.int32, 
                                                              minimum=0, name='observation')
-        self._state = 0
-        self._episode_ended = False
-        self._tries = 0
-        self._solution = np.random.randint(0, 6)
+       
+        self._reset_game()       
+        
 
     def action_spec(self):
         return self._action_spec
@@ -41,12 +41,15 @@ class ClueCardCategoryEnv(py_environment.PyEnvironment):
     def observation_spec(self):
         return self._observation_spec
 
-    def _reset(self):
-        self._state = 0
+    def _reset_game(self):
+        self._state = np.zeros((self._num_of_cards,), dtype=np.int32)
         self._tries = 0
-        self._episode_ended = False
         self._solution = np.random.randint(0, 6)
-        return ts.restart(np.array([self._state], dtype=np.int32))
+        self._episode_ended = False
+
+    def _reset(self):        
+        self._reset_game()
+        return ts.restart(self._state)
 
     def _step(self, action):
         if self._episode_ended:
@@ -55,28 +58,33 @@ class ClueCardCategoryEnv(py_environment.PyEnvironment):
             return self.reset()
 
         self._tries += 1
-        print(action)
-
+        
+        action = action[0]
+        
         # Make sure episodes don't go on forever.
         if action >= 0 and action < self._num_of_cards:
-            self._guess(action)
+            correct = self._guess(action)
         else:
             raise ValueError('`action` should be between 0 and ' + str(self._num_of_cards))
 
-        if self._episode_ended or self._state == 1 or self._tries == 10:
+        if self._episode_ended or correct or self._tries == 10:
             reward = 10 - self._tries
-            return ts.termination(np.array([self._state], dtype=np.int32), reward)
+            return ts.termination(self._state, reward)
         else:
-            return ts.transition(np.array([self._state], dtype=np.int32), reward=0.0, discount=1.0)
+            return ts.transition(self._state, reward=0.0, discount=1.0)
 
     def _handle_guess(self):
         pass
 
-    def _guess(self, action):
-        if action == self._solution:
-            self._state = 1
+    def _guess(self, action) -> bool:
+        self._state[action] += 1
+
+        return action == self._solution
+
+
+if __name__ == "__main__":
+    environment = ClueCardCategoryEnv()
+    utils.validate_py_environment(environment, episodes=5)
 
 
 
-environment = ClueCardCategoryEnv()
-utils.validate_py_environment(environment, episodes=5)
