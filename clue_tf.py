e33ffc80811f50bc05f359735d469af9df8f40fb
jeff012345 <jtmontic@gmail.com>
2020-10-25 16:18:44 -0400

    hyperparameter tuning

--- clue_tf.py
+++ clue_tf.py
@@ -57,21 +57,20 @@ def collect_data(env, policy, buffer, steps):
   for _ in range(steps):
     collect_step(env, policy, buffer)
 
-
 ##
 ## Hyperparameters
 ##
-num_iterations = 50000 # @param {type:"integer"}
+num_iterations = 1000000 # @param {type:"integer"}
 
-initial_collect_steps = 100  # @param {type:"integer"} 
-collect_steps_per_iteration = 1  # @param {type:"integer"}
+initial_collect_steps = 1000  # @param {type:"integer"} 
+collect_steps_per_iteration = 10  # @param {type:"integer"}
 replay_buffer_max_length = 100000  # @param {type:"integer"}
 
 batch_size = 64  # @param {type:"integer"}
 learning_rate = 1e-3  # @param {type:"number"}
 log_interval = 200  # @param {type:"integer"}
 
-num_eval_episodes = 10  # @param {type:"integer"}
+num_eval_episodes = 25  # @param {type:"integer"}
 eval_interval = 1000  # @param {type:"integer"}
 
 ##
